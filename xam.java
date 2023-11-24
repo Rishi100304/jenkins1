97e33c70e7aadffc47c4dc72eb5d6a75fe061374
Rishi100304 <rishianand1018@gmail.com>
2023-11-24 14:03:44 +0530

    first commit

---
+++ xam.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class First
+{
+    public static void main(String args[])
+    {
+        int a=10, b=20;
+        int c=a*b;
+        System.out.println("Multiplication of a and b = "+c);
+    }
+}
